8a3b7b1da425b8dcb6a656cef871033a962219b0
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-09-20 12:53:42 +1000

    Corrected instance logging.

--- ckanext/odi_certificates_store/jobs.py
+++ ckanext/odi_certificates_store/jobs.py
@@ -4,7 +4,7 @@ import ckan.lib.jobs as jobs
 import ckan.plugins.toolkit as t
 from ckan import model
 
-g_log = getLogger(__name__)
+log = getLogger(__name__)
 
 
 class Certificates_Importer(object):
@@ -17,11 +17,12 @@ class Certificates_Importer(object):
         return cls._instance
 
     def __init__(self):
-        self.log = getLogger(__name__)
+        self.log = getLogger("ckan.lib.cli")
 
     def odi_certificates_all_jobs(self, jobs_fn):
         try:
             jobs.enqueue(odi_certificates_all_process, [jobs_fn])
+            self.log.info("Odi certificate all jobs queued: %s", str(jobs_fn))
         except ValueError, e:
             self.log.error(e)
 
@@ -30,28 +31,27 @@ class Certificates_Importer(object):
             self.odi_certificate_job(data_id, job_fn)
 
     def odi_certificate_job(self, id, job_fn):
-        self.log.info("Odi 1 certificate job queued: %s", id)
         try:
             jobs.enqueue(job_fn, [id])
-            g_log.info("Odi certificate job queued: %s", id)
+            self.log.info("Odi certificate job queued: %s", id)
         except ValueError, e:
             self.log.error(e)
 
 
 def odi_certificates_all_process(post_process_fn):
-    g_log.debug("Gathering all certificates...")
+    log.debug("Gathering all certificates...")
     responses = t.get_action('odi_certificates_get_all')(ckan_context())
     if responses.status_code == 200:
         responses_json = responses.json()
-        g_log.debug('Import all response: %r', responses)
+        log.debug('Import all response: %r', responses)
         for response_json in responses_json.get('certificates', {}):
             try:
                 _validate_location(response_json)
                 post_process_fn(response_json)
             except t.ValidationError as e:
-                g_log.error("Unable to proceed further with this record. Reason: %s", e)
+                log.error("Unable to proceed further with this record. Reason: %s", e)
     else:
-        g_log.info("Import aborted for all certificates jobs. Reason: %r", responses)
+        log.info("Import aborted for all certificates jobs. Reason: %r", responses)
 
 
 def odi_certificate_level_import(id):
@@ -59,26 +59,26 @@ def odi_certificate_level_import(id):
         response = t.get_action('odi_certificate_get_from_id')(ckan_context(), {'id': id})
         _process_odi_certificate_response(response, id, odi_certificate_level_update)
     except t.ValidationError as e:
-        g_log.error("Unable to proceed further with this record. Reason: %s", e)
+        log.error("Unable to proceed further with this record. Reason: %s", e)
 
 
 def _process_odi_certificate_response(response, id, post_process_fn):
-    g_log.debug('import response is %r', response)
+    log.debug('import response is %r', response)
     if response.status_code == 200:
         response_json = response.json()
         certificate_json = t.get_or_bust(response_json, 'certificate')
         certificate_json['id'] = id
-        g_log.debug('response post-process is now: %r', certificate_json)
+        log.debug('response post-process is now: %r', certificate_json)
         post_process_fn(certificate_json)
     else:
-        g_log.info("Import aborted for %s. Reason: %r", id, response)
+        log.info("Import aborted for %s. Reason: %r", id, response)
 
 
 def odi_certificate_level_update(data_dict):
-    g_log.debug("Sending data for level update: %r", data_dict)
+    log.debug("Sending data for level update: %r", data_dict)
     context = ckan_context()
     response = t.get_action('odi_certificate_level_as_extra_update')(context, data_dict=data_dict)
-    g_log.info('Import response is %r' % response)
+    log.info('Import response is %r' % response)
     return response
 
 
@@ -86,12 +86,12 @@ def _validate_location(response_json):
     # because an odi user can change the data title, \
     # we cannot rely on this from the response set. \
     # We need to go back and check for id against the documentation url
-    g_log.info("Gathering data for %s", response_json['title'])
+    log.info("Gathering data for %s", response_json['title'])
     location = _location(response_json)
-    g_log.debug("Location found is: %s", location)
+    log.debug("Location found is: %s", location)
     documentation_url = _documentation_url(response_json)
     dataset_id = _guess_dataset_id(documentation_url)
-    g_log.info("Trying %s as ckan dataset id...", dataset_id)
+    log.info("Trying %s as ckan dataset id...", dataset_id)
     _validate_dataset_id(dataset_id, location)
     response_json['id'] = dataset_id
 
@@ -121,14 +121,14 @@ def _is_dataset_id_valid_for_location(dataset_id, location):
         response_json = response.json()
         certificate = response_json.get('certificate', {})
         location_from_id = _location(certificate)
-        g_log.debug("Certificate location captured from dataset_id: %s, is: %s", dataset_id, location_from_id)
+        log.debug("Certificate location captured from dataset_id: %s, is: %s", dataset_id, location_from_id)
         return location == location_from_id
     else:
         raise t.ValidationError("Unable to validate dataset id: %s. Reason: %r", id, response)
 
 
 def _location(certificate):
-    g_log.debug('checking location(uri) in certificate: %r', certificate)
+    log.debug('checking location(uri) in certificate: %r', certificate)
     location = t.get_or_bust(certificate, 'uri')
     return location + '.json'
 
